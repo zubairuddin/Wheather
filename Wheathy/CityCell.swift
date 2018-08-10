d199d2801c7480a77cc9d9c1854d4c1a88fa78f3
Zubair.Nagori <zubair.nagori@ideavate.com>
2018-08-10 16:12:43 +0530

    Initial Commit.

---
+++ Wheathy/CityCell.swift
@@ -0,0 +1,25 @@
+//
+//  CityCell.swift
+//  Clima
+//
+//  Created by Zubair.Nagori on 06/08/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+
+    @IBOutlet weak var lblCityName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Wheathy/WeatherViewController.swift
@@ -0,0 +1,165 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Angela Yu on 23/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate, ChangeCityDelegate {
+    
+    @IBOutlet weak var faren: UISwitch!
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "23030bfcf6b163c0e6ab7a0d6fdfe610"
+    
+    @IBAction func `switch`(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            
+        }
+    }
+    
+    //TODO: Declare instance variables here
+    let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
+ 
+    
+    
+    //Pre-linked IBOutlets
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        //TODO:Set up the location manager here.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+       
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    
+    func getWeatherData(url: String, parameters: [String: String]) {
+        
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                
+                print("Success! Got the weather data")
+                let weatherJSON : JSON = JSON(response.result.value!)
+                
+                
+                print(weatherJSON)
+                
+                self.updateWeatherData(json: weatherJSON)
+                
+            }
+            else {
+                print("Error \(String(describing: response.result.error))")
+                self.cityLabel.text = "Connection Issues"
+            }
+        }
+        
+    }
+    
+    @IBAction func changeCityAction(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "ChangeCityViewController") as! ChangeCityViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func updateWeatherData(json : JSON) {
+    
+        let tempResult = json["main"]["temp"].doubleValue
+    
+            weatherDataModel.temperature = Int(tempResult - 273.15)
+    
+            weatherDataModel.city = json["name"].stringValue
+    
+            weatherDataModel.condition = json["weather"][0]["id"].intValue
+    
+            weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+    
+    
+            updateUIWithWeatherData()
+    }
+    
+    func updateUIWithWeatherData() {
+        
+        cityLabel.text = weatherDataModel.city
+        temperatureLabel.text = "\(weatherDataModel.temperature)°"
+        weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
+        
+    }
+    
+    //MARK: - Location Manager Delegate Methods
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            
+            self.locationManager.stopUpdatingLocation()
+            
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+            
+            getWeatherData(url: WEATHER_URL, parameters: params)
+        }
+    }
+    
+    
+    //Write the didFailWithError method here:
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
+
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    
+    
+    func userEnteredANewCityName(city: String) {
+        let params : [String : String] = ["q" : city, "appid" : APP_ID]
+        getWeatherData(url: WEATHER_URL, parameters: params)
+    }
+
+    
+    //Write the PrepareForSegue Method here
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "changeCityName" {
+            let destinationVC = segue.destination as! ChangeCityViewController
+            destinationVC.delegate = self
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
